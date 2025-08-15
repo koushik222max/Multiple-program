f25f2bb9b317a9ca59d783e6be057e2d22df9cdb
Koushik Raj <koushikraj768@gmail.com>
2025-08-15 11:42:36 +0530

    Add files via upload

---
+++ for testing.py
@@ -0,0 +1,51 @@
+#a=int(input('enter the even number:'))
+
+#while a%2!=0:
+    #print('---->please enter the even number')
+    #a=int(input('enter the even number:'))
+
+#print(f'you entered number:{a}')
+
+
+
+#def koushik():
+    #print('hi how are you\nhave a nice day')
+#print('some block of code')
+#koushik()
+
+#print('new block of code')
+#koushik()
+
+def add(fn,sn):
+    result=(fn+sn)
+    print('result','=',result)
+
+add(11,22)
+
+
+def getsum(*x):
+    result=0
+    for a in x:
+        result+=a
+    print('result','=',result)
+
+getsum(12,23,34,56,78,90,21,23,3,4,5,6,7,6,7,89,98,75,42)
+
+gv=2
+
+def sum(fn):
+    print(gv+fn)
+
+sum(4)
+
+
+
+
+
+
+
+
+
+
+
+    
